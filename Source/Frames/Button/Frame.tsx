2d30ca8d98305b28fa18f1806a9575b7400f93e7
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2024-06-19 18:41:44 -0400

    Tested out webp data URIs

--- Source/Frames/Button/Frame.tsx
+++ Source/Frames/Button/Frame.tsx
@@ -7,6 +7,14 @@ import { Session } from '../../Misc/Types.ts'
 import { render } from 'Render';
 
 
+import { encodeBase64 } from "https://deno.land/std@0.224.0/encoding/base64.ts";
+
+const buffer = await Deno.readFile(`./Source/Static/Icons/Reaction.webp`)
+
+const uri = `data:image/webp;base64,${ encodeBase64(buffer) }`
+
+
+
 interface Args {
 
     onClick :
@@ -21,14 +29,14 @@ const Frame =
     ( context : FrameContext ) =>
     ( args : Args ) => {
 
-    const icon = `/Asset/Icons/${ args.icon }.webp`
+    // const icon = `/Asset/Icons/${ args.icon }.webp`
 
     const { content , style , slug } = context
 
     return (
         <div class = 'Button' >
 
-            <img src = { icon } />
+            <img src = { uri } />
 
             <iframe srcDoc = { render(content({ ... args , style , slug })) } />
 
